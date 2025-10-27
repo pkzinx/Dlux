6981edefcbf8b86b6a5dc42b1a30834e8f7d139c
pkzinx <kaiopietrodl@gmail.com>
2025-10-27 16:35:11 -0300

    Fix TypeScript error in tests helpers

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -1,7 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 import { render, RenderResult } from '@testing-library/react';
+import React from 'react';
 
 import theme from '~src/styles/theme';
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
+  // @ts-ignore
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
